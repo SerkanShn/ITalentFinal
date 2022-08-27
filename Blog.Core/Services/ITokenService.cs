b74cc4fdd80d3728631e5bedf4ebb9b6883fa650
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-27 23:22:07 +0300

    TokenDTO ve ITokenService oluşturuldu , UserApp erişim belirleyicisi değiştirildi

---
+++ Blog.Core/Services/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Core.DTOs;
+using Blog.Core.Entities.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDTO CreateToken(UserApp userApp);
+        
+    }
+}
