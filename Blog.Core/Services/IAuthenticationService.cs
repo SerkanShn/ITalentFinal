35a1eeda124b336d1fc586ea4ca4c2f18c9cc0ba
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-27 23:23:06 +0300

    LoginDTO ve IAuthenticate oluşturuldu

---
+++ Blog.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        CustomResponse<TokenDTO> CreateToken(LoginDTO loginDTO);
+        CustomResponse<TokenDTO> CreateTokenByRefreshToken(string refreshToken);
+        void DeleteRefreshToken(string refreshToken);
+    }
+}
