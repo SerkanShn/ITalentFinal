409b1f44a1bdf98ccf03e46481cc3a8e0fb27866
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-27 22:18:27 +0300

    Category,Comment,Post entityleri eklendi

---
+++ Blog.Core/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public bool IsPublish { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Title { get; set; }
+        public string CommentContent { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+
+    }
+}
---
+++ Blog.Core/Entities/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string PostBanner { get; set; }
+        public string PostContent { get; set; }
+        public bool IsPublish { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? EditedOn { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+
+    }
+}
