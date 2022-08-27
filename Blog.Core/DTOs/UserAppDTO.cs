79e7245b08057118de325e814018cef21ca7dabb
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-27 23:23:50 +0300

    IUserService interfacei oluşturuldu, CreateUserDTO ve UserAppDTO oluşturuldu

---
+++ Blog.Core/DTOs/UserAppDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.DTOs
+{
+    public class UserAppDTO
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Blog.Core/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Services
+{
+    public interface IUserService
+    {
+        CustomResponse<UserAppDTO> CreateUser(CreateUserDTO createUserDTO);
+        CustomResponse<UserAppDTO> GetUserByName(string username);
+    }
+}
