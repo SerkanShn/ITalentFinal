84d82ba9e01ec52a30cca19ed30d4bf650bc5798
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-27 22:40:35 +0300

    IGenericRepository eklendi

---
+++ Blog.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(int Id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Delete(int id);
+        void Update(T entity);
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+        bool Any(Expression<Func<T, bool>> expression);
+    }
+}
