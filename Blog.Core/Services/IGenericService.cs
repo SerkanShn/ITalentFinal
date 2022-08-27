18647df41925606f0de5817306fc64e2ef43f9b6
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-27 22:33:39 +0300

    IGenericService interfacei eklendi

---
+++ Blog.Core/Services/IGenericService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Services
+{
+    public interface IGenericService<T,TDTO> where T : class where TDTO : class
+    {
+        List<CustomResponse<TDTO>> GetList();
+
+        CustomResponse<TDTO> GetById(int id);
+
+        CustomResponse<TDTO> Create(T entity);
+
+        void Update(T entity);
+
+        void Delete(int id);
+        CustomResponse<IEnumerable<TDTO>> Where(Expression<Func<T, bool>> predicate);
+        bool Any(Expression<Func<T, bool>> expression);
+    }
+}
