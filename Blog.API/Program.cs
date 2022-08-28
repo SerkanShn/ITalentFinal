846d8e119f5d15f703a4ba0953247bfc182ed323
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-28 14:30:16 +0300

    Token ayarları ve ilgili class oluşturuldu

--- Blog.API/Program.cs
+++ Blog.API/Program.cs
@@ -1,3 +1,5 @@
+using Blog.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Configuration.GetSection("TokenOption").Get<AppTokenOption>();
 
 var app = builder.Build();
 
---
+++ Blog.Core/AppTokenOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core
+{
+    public class AppTokenOption
+    {
+        public string Issuer { get; set; }
+        public int AccessTokenExpiration { get; set; }
+        public int RefreshTokenExpiration { get; set; }
+        public string SecurityKey { get; set; }
+    }
+}
