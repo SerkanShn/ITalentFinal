2752946830d8a992e2c960f648b16a377c8a1413
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-28 12:17:41 +0300

    Mapper Oluşturuldu

---
+++ Blog.Service/MapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Blog.Core.DTOs;
+using Blog.Core.Entities.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Service
+{
+    internal class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<UserApp, UserAppDTO>().ReverseMap();
+
+        }
+    }
+}
