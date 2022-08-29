157b0f12ce20353f02ff11323d0e9f80d77df52e
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-29 19:38:44 +0300

    CommentConfiguration eklendi

---
+++ Blog.Repository/Configurations/CommentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Repository.Configurations
+{
+    internal class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.HasOne(x => x.Post).WithMany(x => x.Comments).HasForeignKey(x=>x.PostId).OnDelete(DeleteBehavior.Restrict);
+            builder.HasOne(x => x.User).WithMany(x => x.Comments).HasForeignKey(x=>x.UserId).OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
