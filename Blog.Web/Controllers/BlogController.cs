3cb1fd0f70d3b68aa73d32ef45a37224ebe0bf41
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-30 22:46:50 +0300

    Blog Controller eklendi

---
+++ Blog.Web/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using Blog.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+
+        private readonly IPostService _postService;
+        public BlogController( IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Post(int id)
+        {
+            var post =await _postService.GetById(id);
+
+            ViewBag.post = post;
+
+            return View();
+        }
+    }
+}
